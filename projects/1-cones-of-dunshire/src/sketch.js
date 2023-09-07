18b1b2aba8b899978d88c0a28ee914c1f63c0463
Michael Sorensen <msorensen388@gmail.com>
2023-09-07 00:30:38 -0400

    messing around

--- projects/1-cones-of-dunshire/src/sketch.js
+++ projects/1-cones-of-dunshire/src/sketch.js
@@ -1,95 +1,92 @@
-import p5 from 'p5';
-
-
 export const s = sketch => {
 
-  var screenWidth = 500,
+  const screenWidth = 500,
     screenHeight = 500;
 
-  var frameCounter = 0;
-  const numberCircles = 5;
-  const xIncrement = screenWidth / numberCircles;
-  const yIncrement = screenHeight / numberCircles;
+  let frameCounter = 0;
+  const numberCircles = 10;
 
-  var increasing = true;
+  // distance between circles
+  const xGap = screenWidth / numberCircles,
+    yGap = screenHeight / numberCircles;
 
-  var r = 0,
+  console.log(xGap);
+
+  let increasing = true;
+
+  let r = 0,
     g = 0,
     b = 0;
 
-  var x = 0,
+  let x = -screenWidth / 2,
     y = 0,
     diameter = 0;
 
-  function calculateColor(a, b, c, range) {
+  const calculateColor = (a, b, c, range) => {
     r++;
     g+=2;
     b--;
 
-    if (a > range) {
-        r = 0;
-    }
-    if (b > range) {
-        g = 0;
-    }
-    if (c < 0) {
-        b = range;
-    }
+    if (a > range) r = 0;
+    if (b > range) g = 0;
+    if (c < 0) b = range;
 
     return { r, g, b };
-  }
-  function calculateCircle(x, y, diameter, increasing) {
-      x++;
-      y--;
+  };
 
-      if (increasing) {
-          diameter++;
-      } else {
-          diameter--;
-      }
+  const calculateCircle = (x, y, diameter, increasing) => {
+    x++;
+    y--;
 
-      if (x > screenWidth || x < 0) {
-          x = 0;
-      }
-      if (y > screenHeight || y < 0) {
-          y = screenHeight;
-      }
+    if (increasing) {
+      diameter++;
+    } else {
+      diameter--;
+    }
 
-      return { x, y, diameter };
-  }
+    if (x > screenWidth || x < 0) x = 0;
+    if (y > screenHeight || y < 0) y = screenHeight;
+
+    return { x, y, diameter };
+  };
 
   sketch.setup = () => {
-      sketch.createCanvas(screenWidth, screenHeight);
+    sketch.createCanvas(screenWidth * 2, screenHeight * 2);
   };
 
   sketch.draw = () => {
-      frameCounter++;
+    frameCounter++;
 
-      if (frameCounter === 300) {
-          sketch.background(220);
+    if (frameCounter === 300) {
+      sketch.clear();
+      sketch.background(220);
+    }
+
+    for (var i = 0; i < numberCircles; i++) {
+      const newRGB = calculateColor(r, g, b, 255);
+      r = newRGB.r;
+      g = newRGB.g;
+      b = newRGB.b;
+
+      if (diameter === 100) {
+        increasing = false;
+      }
+      if (diameter === 0) {
+        increasing = true;
       }
 
-      for (var i = 0; i < numberCircles; i++) {
-          const newRGB = calculateColor(r, g, b, 255);
-          r = newRGB.r;
-          g = newRGB.g;
-          b = newRGB.b;
+      const newXY = calculateCircle(x, y, diameter, increasing);
+      x = newXY.x;
+      y = newXY.y;
+      diameter = newXY.diameter;
 
-          if (diameter === 100) {
-              increasing = false;
-          }
-          if (diameter === 0) {
-              increasing = true;
-          }
+      sketch.fill(r, g, b);
 
-          const newXY = calculateCircle(x, y, diameter, increasing)
-          x = newXY.x;
-          y = newXY.y;
-          diameter = newXY.diameter;
-
-          sketch.fill(r, g, b);
-          sketch.circle(x + xIncrement * i, y + yIncrement * i, diameter);
+      if (i < numberCircles / 2) {
+        sketch.circle(x, y + yGap * i, diameter);
+      } else {
+        sketch.circle(x + xGap * i, y + yGap * numberCircles / 2, diameter);
       }
-
-  }
+    }
+  };
 };
